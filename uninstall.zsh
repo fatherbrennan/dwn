e1dd3cd49b19a0b8833ce4fd6a6abef157dd3b53
fatherbrennan <bjt.lepore@gmail.com>
2025-06-04 17:43:26 +0930

    feat: :sparkles: add simple uninstall script to remove env vars

---
+++ uninstall.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+source ./common.zsh
+
+INSTALLER_LOG=uninstall
+
+# unset environment variables from zsh.
+function unset_env_vars() {
+  INSTALLER_STEP=unset_env_vars
+  local env_vars_no_leading_newline="${ENV_VARIABLES_TEXT:2}" # Remove leading newline character `\n`.
+
+  # check if the file exists.
+  if [[ -f "$FILE_ZSHRC" ]]; then
+    _tmp_f_env_vars=$(mktemp)
+    _tmp_f_zshrc=$(mktemp)
+
+    echo "$env_vars_no_leading_newline" >"$_tmp_f_env_vars"
+
+    # remove environment variables from file.
+    grep -xvFf "$_tmp_f_env_vars" "$FILE_ZSHRC" >"$_tmp_f_zshrc" && mv "$_tmp_f_zshrc" "$FILE_ZSHRC"
+  fi
+  log "environment variables unset!"
+}
+
+unset_env_vars
