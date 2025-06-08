4654602211d8e12b21f441cc9d025bd9210aa7cc
fatherbrennan <bjt.lepore@gmail.com>
2025-06-08 13:36:28 +0930

    fix: :ambulance: change stdout broken formatting

--- common.zsh
+++ common.zsh
@@ -36,25 +36,25 @@ ERROR[$ERROR_CONFIG_DIR_REMOVE]='error removing dwn config directory'
 
 # print line.
 function print_ln() {
-  printf '%s\n' "$1"
+  printf "%s\n" "$1"
 }
 
 # print error code and message, and exit with non-zero code.
 # $1 ERROR code.
 function log_error_exit() {
-  print_ln "[$INSTALLER_LOG] $INSTALLER_STEP [ERROR:$1]:\t$ERROR[$1]" && exit 1
+  printf '[%s] %s [ERROR:%s]:\t%s\n' "$INSTALLER_LOG" "$INSTALLER_STEP" "$1" "$ERROR[$1]" && exit 1
 }
 
 # print warning log message.
 # $1 warning message.
 function log_warn() {
-  print_ln "[$INSTALLER_LOG] $INSTALLER_STEP [WARN]:\t$1"
+  printf '[%s] %s [WARN]:\t%s\n' "$INSTALLER_LOG" "$INSTALLER_STEP" "$1"
 }
 
 # print log message.
 # $1 log message.
 function log() {
-  print_ln "[$INSTALLER_LOG] $INSTALLER_STEP\t$1"
+  printf '[%s] %s\t%s\n' "$INSTALLER_LOG" "$INSTALLER_STEP" "$1"
 }
 
 # return expected result of bool function.
